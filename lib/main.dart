1190e4ae79421a54c80ed230ce5e64c1622685b1
Antonio <criticalgeek1@gmail.com>
2019-12-25 19:02:47 -0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,122 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter OCR',
+      home: LandingScreen(),
+    );
+  }
+}
+
+class LandingScreen extends StatefulWidget {
+  @override
+  _LandingScreenState createState() => _LandingScreenState();
+}
+
+class _LandingScreenState extends State<LandingScreen> {
+  File imageFile;
+
+  _openGallery(BuildContext context) async {
+    var picture = await ImagePicker.pickImage(source: ImageSource.gallery);
+    this.setState(() {
+      imageFile = picture;
+    });
+    Navigator.of(context).pop();
+  }
+
+  _openCamera(BuildContext context) async {
+    var picture = await ImagePicker.pickImage(source: ImageSource.camera);
+    this.setState(() {
+      imageFile = picture;
+    });
+    Navigator.of(context).pop();
+  }
+
+  _ocr(BuildContext context) async {
+    var url = 'https://jsonplaceholder.typicode.com/posts';
+    var response = await http
+        .post(url, body: {'title': 'foo', 'body': 'bar', 'userId': '1'});
+
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+    print(await http.read('https://jsonplaceholder.typicode.com/posts/1'));
+  }
+
+  Future<void> _showChoiceDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Make a choice'),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  GestureDetector(
+                    child: Text('Galeria'),
+                    onTap: () {
+                      _openGallery(context);
+                    },
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                  ),
+                  GestureDetector(
+                    child: Text('Camera'),
+                    onTap: () {
+                      _openCamera(context);
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+  Widget _decideImageView() {
+    if (imageFile == null) {
+      return Text('No image Selected');
+    } else {
+      return Image.file(imageFile, width: 400, height: 400);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('OCR - Industry'),
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              _decideImageView(),
+              RaisedButton(
+                onPressed: () {
+                  _showChoiceDialog(context);
+                },
+                child: Text('Select Image'),
+              ),
+              RaisedButton(
+                onPressed: () {
+                  _ocr(context);
+                },
+                child: Text('OCR Image'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
